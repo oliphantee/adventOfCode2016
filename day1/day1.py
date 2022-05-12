fb78f19ce9a2708e739ab65ee0f06e5178db02e7
Ephraim Oliphant <eeo003@bucknell.edu>
2022-05-12 15:58:35 -0400

    finished day2 and day1

---
+++ day1/day1.py
@@ -0,0 +1,30 @@
+file = open("day1.txt")
+
+line=file.readline()
+line=line.split(", ")
+
+visited=set()
+locX=0
+locY=0
+easterLoc=None
+heading=[1,0]
+for dir in line:
+    turn=dir[0]
+    dist=dir[1:]
+    if turn=="R":
+        newX=heading[1]*-1
+        newY=heading[0]
+        heading=[newX,newY]
+    elif turn=="L":
+        newX=heading[1]
+        newY=heading[0]*-1
+        heading=[newX,newY]
+    for _ in range(int(dist)):
+        if (locX,locY) in visited and easterLoc==None:
+            easterLoc=(locX,locY)
+        visited.add((locX,locY))
+        locX+=heading[0]
+        locY+=heading[1]
+
+print(abs(locX)+abs(locY)) # part 1
+print(abs(easterLoc[0])+abs(easterLoc[1])) # part 2
---
+++ day2/day2.py
@@ -0,0 +1,52 @@
+file=open("day2.txt")
+
+# part 1 below
+
+# def getVal(row,col):
+#     return col+row*3+1
+#
+#
+# row=1
+# col=1
+#
+# for line in file.readlines():
+#     for char in line.rstrip():
+#         if char=="L" and col>0:
+#             col-=1
+#         if char=="R" and col<2:
+#             col+=1
+#         if char=="U" and row>0:
+#             row-=1
+#         if char=="D" and row<2:
+#             row+=1
+#     print(getVal(row,col))
+
+# part 2
+
+row=2
+col=0
+
+def getVal(row,col): # this is messy, a formula would probably be better
+    if row==0:
+        return 1
+    if row==1:
+        return col+1
+    if row==2:
+        return col+5
+    if row==3:
+        return col+9
+    if row==4:
+        return 13
+
+for line in file.readlines():
+    for char in line.rstrip():
+        val=getVal(row,col)
+        if char=="L" and val not in [1,2,5,10,13]: # could generalize this by having edge list variables
+            col-=1
+        if char=="R" and val not in [1,4,9,12,13]:
+            col+=1
+        if char=="U" and val not in [5,2,1,4,9]:
+            row-=1
+        if char=="D" and val not in [5,10,13,12,9]:
+            row+=1
+    print(getVal(row,col)) # part 2
